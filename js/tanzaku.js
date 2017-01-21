538400ec99b35268cf259b2c1783c610b9c7ec9e
松本嵩大 <takahiro91687716@yahoo.co.jp>
2017-01-21 22:27:15 +0900

    [Add]ドロップするエリアの追加
    課題
    *ドラッグドロップした時に持ってきた元が消えないようにする
    *解答欄から外へドラッグドロップしたときに選択肢を消せる
    *選択肢を並び替えられる

--- js/tanzaku.js
+++ js/tanzaku.js
@@ -1,5 +1,9 @@
 //XMLHttpRequestオブジェクトを生成
 var xhr = new XMLHttpRequest();
+var lns = [];
+var lst = [];
+var pre = 0, idc = 1;
+var unit = 0;
 
 //--------------------------------------------------
 //
@@ -16,6 +20,10 @@ function requestFile(method, fname, async) {
 		}
 	}
 	xhr.send(null);
+	
+	itemDrag();
+	//dropAction();
+	canvasAction();
 }
 
 //--------------------------------------------------
@@ -33,11 +41,27 @@ function buildQuestion(HttpObj){
 	document.getElementById("question").innerHTML += "<p>" + textList[0].childNodes[0].nodeValue + " </p>";
 	
 	//選択肢領域
-	document.getElementById("choices").innerHTML += "<h3>選択肢<h3>";
+	document.getElementById("choices").innerHTML += "<h3>選択肢</h3>";
 	for(i = 0; i < itemList.length; i++) {
-		document.getElementById("choices").innerHTML += "<div id= item"+ i +">";
-		document.getElementById("choices").innerHTML += itemList[i].childNodes[0].nodeValue + "</div>";
+		var str = "<div id= \"i"+ i +"\" draggable = \"true\"  ondragstart=\"itemDragStart(event)\">" ;
+		str += buildChoicesParts(itemList[i]);
+		str += "</div>";
+		document.getElementById("choices").innerHTML += str;
+		
+		lst[i] = buildChoicesParts(itemList[i]);
 	}
+}
+
+function buildChoicesParts(item){
+	str = item.childNodes[0].nodeValue;
+	
+	//正規表現を使ってTagを識別する予定
+	//再帰的に呼び出すメソッドも必要か
+	//var regexp = /&lt;+.!+&gt/i;
+	//if(item.getElementsByTagName('stdio.h') !=  null){
+	//	str += "&lt;stdio.h&gt;";
+	//}
+	return str;
 }
 
 //--------------------------------------------------
@@ -48,4 +72,43 @@ function buildAnswerArea(){
 	
 	
 	
+}
+
+var bdy;
+function itemDrag(){
+	bdy = document.getElementById('d');
+	bdy.ondragover = prev;
+	bdy.ondrop = function(e) {
+		var id = e.dataTransfer.getData('text');
+		var elt = document.getElementById(id);
+		//if(id[0] == 'x') {
+		//	rmelt(elt); adjindent(); prev(e);
+		//}
+	}
+}
+
+//選択肢をドラッグした時に
+//データを渡す
+function itemDragStart(e) {
+  e.dataTransfer.setData('text',e.target.id);
+}
+
+//キャンバスに追加する
+function canvasAction(){
+	canvas = document.getElementById('canvas');
+	
+	canvas.ondragover = prev;
+	canvas.ondrag=prev;
+	canvas.ondrop = function(e){
+		id = e.dataTransfer.getData('text');
+		var elt = document.getElementById(id)
+		alert(document.getElementById(id));
+		canvas.appendChild(document.getElementById(id));
+	}
+}
+
+function prev(e) {
+	if(e.preventDefault) {
+		e.preventDefault();
+	}
 }
